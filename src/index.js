b7438dc1e4f43621c5b793f2536f3d6948ba2309
kuldeep <kuldeep.phppoets@gmail.com>
2023-04-18 16:17:12 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import * as serviceWorker from "./serviceWorker"
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 import store from "./store"
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
-)
+  </Provider>,
+  document.getElementById('root')
+);
+
 
 serviceWorker.unregister()
